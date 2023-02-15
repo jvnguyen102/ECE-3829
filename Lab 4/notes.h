2c5fab5ccc0ff509b94220a5fae1058d01bd6887
Jacob Nguyen <123837162+jvnguyen102@users.noreply.github.com>
2023-02-15 10:18:11 -0500

    Create notes.h

---
+++ Lab 4/notes.h
@@ -0,0 +1,37 @@
+/* notes.h
+ *  Note Frequencies
+ */
+#ifndef NOTES_H_
+#define NOTES_H_
+
+#define C3 130.81
+#define CS3 139
+#define D3 146.83
+#define DS3 156
+#define E3 164.81
+#define F3 174.61
+#define FS3 185
+#define G3 196.00
+#define GS3 208
+#define A3 220.00
+#define AS3 233
+#define B3 246.94
+#define C4 261.63
+#define CS4 277
+#define D4 293.66
+#define DS4 311
+#define E4 329.63
+#define F4 349.23
+#define FS4 369.99
+#define G4 392.00
+#define GS4 415
+#define A4 440.00
+#define AS4 466
+#define B4 493.88
+#define C5 523.25
+#define CS5 554
+#define D5 587.33
+#define DS5 622
+
+
+#endif /* NOTES_H_ */
